49fe4bb068ed3797970575c51eb5265da22b086f
Will Cygan <wcygan.io@gmail.com>
2022-03-05 11:37:53 -0600

    added image dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use image::{ImageBuffer, ImageFormat, Rgb};
+use std::path::Path;
+
 mod color;
 mod tuple;
 
 fn main() {
-    println!("Hello, world!");
+    let w = 50;
+    let h = 50;
+    let mut canvas: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(w, h);
+    canvas
+        .save_with_format(&Path::new("canvas.ppm"), ImageFormat::Pnm)
+        .unwrap();
 }
