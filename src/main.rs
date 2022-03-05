57ecfce065c16b924abed6ecdbfc422a0dbfae5f
Will Cygan <wcygan.io@gmail.com>
2022-03-05 11:41:56 -0600

    nit

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let w = 50;
     let h = 50;
     let mut canvas: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(w, h);
-    canvas.put_pixel(40, 23, Rgb([10, 50, 100]));
     canvas
         .save_with_format(&Path::new("canvas.ppm"), ImageFormat::Pnm)
         .unwrap();
