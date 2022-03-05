15ada6d909a6a1196061f6e6527d9b7087e5bf2f
Will Cygan <wcygan.io@gmail.com>
2022-03-04 21:09:45 -0600

    adding and subtracting tuples

--- src/tuple.rs
+++ src/tuple.rs
@@ -31,22 +31,36 @@ pub fn is_point(tuple: (f64, f64, f64, f64)) -> bool {
 }
 
 ///
-/// Determines equality between two tuples
-///
-pub fn eq_tup(t1: (f64, f64, f64, f64), t2: (f64, f64, f64, f64)) -> bool {
-    eq_f64(t1.0, t2.0) &&
-        eq_f64(t1.1, t2.1) &&
-        eq_f64(t1.2, t2.2) &&
-        eq_f64(t1.3, t2.3)
-}
-
-///
 /// Determines equality between two 64-bit floating-point values
 ///
 pub fn eq_f64(left: f64, right: f64) -> bool {
     (left - right).abs() < EPSILON
 }
 
+///
+/// Determines equality between two tuples
+///
+pub fn eq_tup(left: (f64, f64, f64, f64), right: (f64, f64, f64, f64)) -> bool {
+    eq_f64(left.0, right.0) &&
+        eq_f64(left.1, right.1) &&
+        eq_f64(left.2, right.2) &&
+        eq_f64(left.3, right.3)
+}
+
+///
+/// Adds two tuples together
+/// WARNING: Behavior is undefined when adding two points together
+///
+pub fn add_tup(left: (f64, f64, f64, f64), right: (f64, f64, f64, f64)) -> (f64, f64, f64, f64) {
+    (left.0 + right.0, left.1 + right.1, left.2 + right.2, left.3 + right.3)
+}
+
+///
+/// Subtracts the right tuple from the left tuple
+///
+pub fn sub_tup(left: (f64, f64, f64, f64), right: (f64, f64, f64, f64)) -> (f64, f64, f64, f64) {
+    (left.0 - right.0, left.1 - right.1, left.2 - right.2, left.3 - right.3)
+}
 
 #[cfg(test)]
 mod tests {
@@ -152,5 +166,85 @@ mod tests {
     fn f64_is_ne() {
         let (x, y) = (1.0, 1.1);
         assert!(!eq_f64(x, y))
+    }
+
+    #[test]
+    fn add_vector_and_point() {
+        let (x1, y1, z1) = (1.0, 2.0, 3.0);
+        let (x2, y2, z2) = (10.0, 20.0, 30.0);
+
+        let pt = point(x1, y1, z1);
+        let vec = vector(x2, y2, z2);
+        let new_pt = add_tup(pt, vec);
+
+        assert!(is_point(new_pt));
+        assert!(eq_f64(x1 + x2, new_pt.0));
+        assert!(eq_f64(y1 + y2, new_pt.1));
+        assert!(eq_f64(z1 + z2, new_pt.2));
+        assert!(eq_f64(POINT_INDICATOR, new_pt.3));
+    }
+
+    #[test]
+    fn add_vector_and_vector() {
+        let (x1, y1, z1) = (1.0, 2.0, 3.0);
+        let (x2, y2, z2) = (10.0, 20.0, 30.0);
+
+        let v1 = vector(x1, y1, z1);
+        let v2 = vector(x2, y2, z2);
+        let v3 = add_tup(v1, v2);
+
+        assert!(is_vector(v3));
+        assert!(eq_f64(x1 + x2, v3.0));
+        assert!(eq_f64(y1 + y2, v3.1));
+        assert!(eq_f64(z1 + z2, v3.2));
+        assert!(eq_f64(VECTOR_INDICATOR, v3.3));
+    }
+
+    #[test]
+    fn sub_point_and_point() {
+        let (x1, y1, z1) = (1.0, 2.0, 3.0);
+        let (x2, y2, z2) = (10.0, 20.0, 30.0);
+
+        let v1 = point(x1, y1, z1);
+        let v2 = point(x2, y2, z2);
+        let v3 = sub_tup(v1, v2);
+
+        assert!(is_vector(v3));
+        assert!(eq_f64(x1 - x2, v3.0));
+        assert!(eq_f64(y1 - y2, v3.1));
+        assert!(eq_f64(z1 - z2, v3.2));
+        assert!(eq_f64(VECTOR_INDICATOR, v3.3));
+    }
+
+    #[test]
+    fn sub_point_and_vector() {
+        let (x1, y1, z1) = (1.0, 2.0, 3.0);
+        let (x2, y2, z2) = (10.0, 20.0, 30.0);
+
+        let v1 = point(x1, y1, z1);
+        let v2 = vector(x2, y2, z2);
+        let v3 = sub_tup(v1, v2);
+
+        assert!(is_point(v3));
+        assert!(eq_f64(x1 - x2, v3.0));
+        assert!(eq_f64(y1 - y2, v3.1));
+        assert!(eq_f64(z1 - z2, v3.2));
+        assert!(eq_f64(POINT_INDICATOR, v3.3));
+    }
+
+    #[test]
+    fn sub_vector_and_vector() {
+        let (x1, y1, z1) = (1.0, 2.0, 3.0);
+        let (x2, y2, z2) = (10.0, 20.0, 30.0);
+
+        let v1 = vector(x1, y1, z1);
+        let v2 = vector(x2, y2, z2);
+        let v3 = sub_tup(v1, v2);
+
+        assert!(is_vector(v3));
+        assert!(eq_f64(x1 - x2, v3.0));
+        assert!(eq_f64(y1 - y2, v3.1));
+        assert!(eq_f64(z1 - z2, v3.2));
+        assert!(eq_f64(VECTOR_INDICATOR, v3.3));
     }
 }
